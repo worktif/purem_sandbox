8764798f85866dc101a3eac5794d2fd0fa1cac9c
Raman Marozau <raman@worktif.com>
2025-04-29 08:07:45 +0200

    Add Business Source License and bump version to 1.3.8
    Integrated the Business Source License 1.1 header across multiple source files, specifying usage terms and copyright. Updated the project version from 1.3.7 to 1.3.8 to reflect these changes.

--- src/performance_test.py
+++ src/performance_test.py
@@ -1,3 +1,16 @@
+"""
+Business Source License 1.1
+
+Copyright (C) 2025 Raman Marozau, raman@worktif.com
+Use of this software is governed by the Business Source License included in the LICENSE.TXT file and at www.mariadb.com/bsl11.
+
+Change Date: Never
+On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+Additional Use Grant: Free for personal and non-commercial research use only.
+
+SPDX-License-Identifier: BUSL-1.1
+"""
+
 import gc
 import math
 import os
